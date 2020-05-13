1408ddc7e34f26c0ff06320a50ecd3131704d0aa
kmeersman <katie4682@yahoo.com>
2020-05-13 17:53:23 -0600

    added savebtn function

--- script.js
+++ script.js
@@ -39,16 +39,27 @@ $(".time-block").each(function () {
 
 // // loop though textarea to get item from local storage
 // for (var hour in notes) {
-for (var i = 0; i < notes.length; i++) {
-    var dataHour = localStorage.getItem(notes[i]);
-
-}
+// for (var i = 0; i < notes.length; i++) {
+//     var dataHour = localStorage.getItem(notes[i]);
+// }
 //save button - local storage
-//  //eventlistener
-// $(".saveBtn").click(function() {
-//     event.preventDefault();
- 
+ //eventlistener
+$(".saveBtn").click(function() {
+    event.preventDefault();
+    var time = $(this).parent().attr("data-hour");
+    console.log("time is" + time);
+    var value = $(this).siblings(".description").val();
+    console.log(value);
+    localStorage.setItem(time, value);
+});
 
-//     localStorage.setItem();
-// });
+$("#hour 9 .description").val(localStorage.getItem("9"));
+$("#hour 10 .description").val(localStorage.getItem("10"));
+$("#hour 11 .description").val(localStorage.getItem("11"));
+$("#hour 12 .description").val(localStorage.getItem("12"));
+$("#hour 13 .description").val(localStorage.getItem("13"));
+$("#hour 14 .description").val(localStorage.getItem("14"));
+$("#hour 15 .description").val(localStorage.getItem("15"));
+$("#hour 16 .description").val(localStorage.getItem("16"));
+$("#hour 17 .description").val(localStorage.getItem("17"));
 
