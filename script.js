a7995db32ab3a6e9ad3e6ed1592e3cfec71ca3b2
kmeersman <katie4682@yahoo.com>
2020-05-13 11:08:10 -0600

    Started js file with variables and psuedo code

---
+++ script.js
@@ -0,0 +1,30 @@
+var time = moment()
+var notes ={
+    9: "Hello World",
+    10: "Test",
+    11: "Class",
+    12: "Lunch",
+    13: "",
+    14: "",
+    15: "",
+    16: "",
+    17: "",
+}
+//Display date in p id="currentDay" class="lead"
+//updates live date/time
+moment(Date);
+        $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+        
+        
+//for loop for starting time??
+for(var hour in notes)
+//need to compare each hour to current time
+    //  // if else statements
+        
+//save button - local storage?
+//  //eventlistener
+
+//append attributes for color
+//gray is time past
+//red is current time
+//green is future time
