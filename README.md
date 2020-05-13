85624d128be480696e8047833d148c1a7b49c725
kmeersman <katie4682@yahoo.com>
2020-05-13 16:20:57 -0600

    update readme.md, added rows to html, created js file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Work-Day-Scheduler
+## Description
+
+This is a work day scheduler that can be used to add in events/appointments for each hour of the workday, 9am-5pm. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery. It also features a clean and polished user interface and is responsive, ensuring that it adapts to multiple screen sizes.
+
+## Usage
+
+upon opening the scheduler, todays's date automatically populates.  As you scroll down, you can see that it hastime blocks listed for standard business hours.  The time blocks are color coded based on hours that have passed (grey), current hour (red), and future hours (green).  Click on a time block to enter in your event for that particular time.  Once done, click the save button. Continue entering and saving for all events/appointments for the day.  If you close and open back up, your events will be saved and listed for you.
+
+## License
+
+© 2020 Kate Meersman brand. All Rights Reserved.
--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-var time = moment()
-var notes ={
-    9: "Hello World",
-    10: "Test",
-    11: "Class",
-    12: "Lunch",
-    13: "",
-    14: "",
-    15: "",
-    16: "",
-    17: "",
-}
+var time = moment();
+var currentHour = time.format("H");
+
+var notes = {
+  9: "Hello World",
+  10: "Test",
+  11: "Class",
+  12: "Lunch",
+  13: "abc",
+  14: "def",
+  15: "ghi",
+  16: "jkl",
+  17: "done",
+};
 //Display date in p id="currentDay" class="lead"
 //updates live date/time
-moment(Date);
-        $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
-        
-        
-//for loop for starting time??
-for(var hour in notes)
-//need to compare each hour to current time
-    //  // if else statements
-        
-//save button - local storage?
-//  //eventlistener
+const date = moment().format("dddd MMMM Do YYYY");
+$("#currentDay").text(date);
 
-//append attributes for color
-//gray is time past
-//red is current time
-//green is future time
+$(".time-block").each(function () {
+  const currentHour = parseInt(moment().format("H"));
+  const hour = parseInt($(this).attr("data-hour"));
+ 
+  if (hour < currentHour) {
+      //add past class
+      //gray is time past
+      $(this).find("textarea").addClass("past");
+    } else if (hour > currentHour) {
+        //add future class
+        //green is future time
+       $(this).find("textarea").addClass("future");
+    } else {
+        //add current class
+        //red is current time
+       $(this).find("textarea").addClass("present");
+    }
+    console.log(hour, currentHour);
+});
+
+// // loop though textarea to get item from local storage
+// for (var hour in notes) {
+for (var i = 0; i < notes.length; i++) {
+    var dataHour = localStorage.getItem(notes[i]);
+
+}
+//save button - local storage
+//  //eventlistener
+// $(".saveBtn").click(function() {
+//     event.preventDefault();
+ 
+
+//     localStorage.setItem();
+// });
+
